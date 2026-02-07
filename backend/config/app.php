b101ee7256f6bb523ee10146992e05ecc5f1fcba
Zanga Siaka KONATE <zangakonate815@gmail.com>
2026-02-07 11:52:22 +0000

    déploiement

--- backend/config/app.php
+++ backend/config/app.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
 return [
 
     /*
@@ -122,5 +126,28 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+
+
+    Application::configure(basePath: dirname(__DIR__))
+        ->withRouting(
+            web: __DIR__ . '/../routes/web.php',
+            api: __DIR__ . '/../routes/api.php',
+            commands: __DIR__ . '/../routes/console.php',
+            health: '/up',
+        )
+        ->withMiddleware(function (Middleware $middleware) {
+            // Assurez-vous que le middleware CORS est bien global
+            $middleware->append([
+                \Illuminate\Http\Middleware\HandleCors::class,
+            ]);
+
+            // Ou spécifiquement pour l'API
+            $middleware->api(prepend: [
+                \Illuminate\Http\Middleware\HandleCors::class,
+            ]);
+        })
+        ->withExceptions(function (Exceptions $exceptions) {
+            //
+        })->create()
 
 ];
